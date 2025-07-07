157b17890218bd7d2efb806c4ee2788c93fffd15
hudson gabriel <hudsonggg@gmail.com>
2025-07-07 19:47:38 +0000

    precisamos resolver esse problema

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <span className="text-xl font-bold font-headline">
+      <div className="text-xl font-bold font-headline">
         <span className="text-foreground">Fretes</span>
         <span className="bg-gradient-to-r from-[#009B3A] via-[#FFCC29] to-[#0033A0] bg-clip-text text-transparent">
           Brasil
         </span>
-      </span>
+      </div>
     </Link>
   );
 }
