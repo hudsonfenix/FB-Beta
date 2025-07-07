0149313f498b081a2a7ecb867e779c6f084935c4
hudson gabriel <hudsonggg@gmail.com>
2025-07-07 23:30:38 +0000

    volte nossa logo para essa

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
       <div className="text-xl font-bold font-headline">
         <span className="text-foreground">Fretes</span>
-        <span className="bg-gradient-to-r from-[#009B3A] via-[#FFCC29] to-[#0033A0] bg-clip-text text-transparent">
+        <span className="text-primary">
           Brasil
         </span>
       </div>
